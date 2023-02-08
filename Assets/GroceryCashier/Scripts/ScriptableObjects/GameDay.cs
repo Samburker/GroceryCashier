ef0b9f084efa17976ddb8a0ec08b3026a82a700e
Tero Korpela <tero.korpela@gmail.com>
2023-02-08 16:48:59 +0200

    GameDays

---
+++ Assets/GroceryCashier/Scripts/ScriptableObjects/GameDay.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameDay", menuName = "McGreedy/GameDay", order = 110)]
+public class GameDay : ScriptableObject
+{
+    public ShopInventory shopInventory;
+    public ShoppingList shoppingList;
+    [ContextMenuItem("Morning", "StartMorning", order = 0)]
+    [ContextMenuItem("Midday", "StartMidday", order = 1)]
+    [ContextMenuItem("Evening", "StartEvening", order = 2)]
+    public float startTime;
+    [ContextMenuItem("Morning", "EndMorning", order = 10)]
+    [ContextMenuItem("Midday", "EndMidday", order = 11)]
+    [ContextMenuItem("Evening", "EndEvening", order = 12)]
+    public float endTime;
+
+    private float dayOfset = 0.25f;
+    void StartMorning() => startTime = 7f - dayOfset;
+    void StartMidday() => startTime = 14f - dayOfset;
+    void StartEvening() => startTime = 22f - dayOfset;
+    void EndMorning() => endTime = 7f + dayOfset;
+    void EndMidday() => endTime = 14f + dayOfset;
+    void EndEvening() => endTime = 22f + dayOfset;
+}
