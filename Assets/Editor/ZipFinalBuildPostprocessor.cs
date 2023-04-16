d2a3978d1a42c5589dfe2ff6ffad88395341a6e1
Tero Korpela <tero.korpela@gmail.com>
2023-04-16 21:08:49 +0300

    PostProcessBuild, Creates zip after the build is completed

---
+++ Assets/Editor/ZipFinalBuildPostprocessor.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.IO;
+using System.IO.Compression;
+
+public class ZipFinalBuildPostprocessor
+{
+    [PostProcessBuild(1000)]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        // Processing only windows builds
+        if (target != BuildTarget.StandaloneWindows && target != BuildTarget.StandaloneWindows64)
+            return;
+
+        string origTargetFolder = Path.GetFullPath(Path.Combine(pathToBuiltProject, ".."));
+        string newTargetFolder = Path.GetFullPath(Path.Combine(pathToBuiltProject, "..", Application.productName + " " + Application.version));
+        string zipPath = Path.Combine(origTargetFolder, Application.productName + " " + Application.version + ".zip");
+
+        // If target directory exists, deleting and recreating it
+        if (Directory.Exists(newTargetFolder))
+            Directory.Delete(newTargetFolder);
+        Directory.CreateDirectory(newTargetFolder);
+
+        // If target zip exists, deleting it
+        if (File.Exists(zipPath))
+            File.Delete(zipPath);
+
+        // Iterating throug all files
+        foreach (var file in Directory.EnumerateFiles(origTargetFolder))
+        {
+            // Excluding zip files
+            if (Path.GetExtension(file) == ".zip")
+                continue;
+
+            // Moving file
+            File.Move(file, Path.Combine(newTargetFolder, Path.GetFileName(file)));
+        }
+
+        // Iterating throug all folders
+        foreach (var dir in Directory.EnumerateDirectories(origTargetFolder))
+        {
+            // Excluding If this folder is current target or other exclude reasons
+            if (Path.GetFullPath(dir).Equals(newTargetFolder))
+                continue;
+            if (dir.Contains("DontShipIt"))
+                continue;
+            if (dir.Contains("DoNotShip"))
+                continue;
+
+            // Moving folder
+            string lastFolderName = Path.GetFileName(dir);
+            Directory.Move(dir, Path.Combine(newTargetFolder, lastFolderName));
+        }
+
+        // Creating the final zip
+        ZipFile.CreateFromDirectory(newTargetFolder, zipPath, System.IO.Compression.CompressionLevel.Optimal, false, null);
+    }
+
+}
